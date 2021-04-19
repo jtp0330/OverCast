21abc5d4507775927cc144588890fe02cb14b1b6
jtp0330 <jtp0330@gmail.com>
2021-04-18 18:25:07 -0700

    Add to Overcast repo

---
+++ OverCast_Prototype_copy/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runspeed = 40f;
+    public float horizontal_move = 0f;
+    
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //Debug.Log() -> used to print return values to console
+        //get input from player
+        horizontal_move = Input.GetAxisRaw("Horizontal") * runspeed;
+    }
+
+    public void FixedUpdate()
+    {
+        //character movement
+        //move based on float value, boolean->crouch, boolean->jump
+        controller.Move(horizontal_move * Time.fixedDeltaTime, false);
+
+    }
+}
